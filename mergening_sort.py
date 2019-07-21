61d7c21ece4146cc3a24db1ebe35fa8203e9d2c1
Troynev Daniel <dtroynev@mail.ru>
2019-07-21 17:47:27 +0300

    27.07 back

--- mergening_sort.py
+++ mergening_sort.py
@@ -1,1 +1,35 @@
-Dan_works
+def mergening_sort(alist, n):
+    if n > 1:
+        #splitting alist -> blist, clist
+        
+        mid = n // 2
+        blist = alist[:mid]
+        clist = alist[mid:]
+
+        mergening_sort(blist, mid)
+        mergening_sort(clist, n - mid)
+
+        #mergening
+        
+        i = 0 # How many objects from 1list have already transported to alist
+        j = 0 # -||- 2list
+        k = 0 
+        while i < mid and j < n - mid:
+            if blist[i] < clist[j]:
+                alist[k] = blist[i]
+                i += 1
+            else:
+                alist[k] = clist[j]
+                j += 1
+            k += 1
+        while i < mid:
+            alist[k] = blist[i]
+            i += 1
+            k += 1
+        while j < n - mid:
+            alist[k] = clist[j]
+            j += 1
+            k += 1
+a = [1, 100, 52, 566, 2, 6, 1, 55, 3]
+mergening_sort(a, len(a))
+print(a)
